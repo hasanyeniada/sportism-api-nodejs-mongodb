6a129da05781c788671f273280da062fc677839a
Hasan Yeniada <hasanyeniada@gmail.com>
2022-10-14 22:08:39 +0300

    Connected to DB using mongoose.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
@@ -5,6 +6,18 @@ const server = require('./app');
 
 console.log(server.get('env'));
 console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {})
+  .then(con => {
+    console.log(con.connections);
+    console.log('DB Connection successful.');
+});
 
 const port = process.env.PORT;
 server.listen(port, () => {

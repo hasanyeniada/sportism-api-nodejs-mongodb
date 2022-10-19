40909eedb9a358226ba7576f8d3380cad84a10e0
Hasan Yeniada <hasanyeniada@gmail.com>
2022-10-20 01:29:03 +0300

    Global error handling for dev and prod seperated.

--- backend/middlewares/globalErrorHandler.js
+++ backend/middlewares/globalErrorHandler.js
@@ -1,13 +1,43 @@
-const globalErrorHandler = (err, req, resp, next) => {
-  console.log(err.stack);
-
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'fail';
-
-  resp.status(err.statusCode).json({
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
     status: err.status,
-    message: err.message || 'Internal Error!',
+    error: err,
+    message: err.message,
+    stack: err.stack,
   });
 };
 
-module.exports = globalErrorHandler;
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR 💥', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(error, res);
+  }
+};

4274d9a5467e298b25f19b1479b6163f7fb2f6a2
Hasan Yeniada <hasanyeniada@gmail.com>
2022-10-21 02:02:25 +0300

    Mongo errors handled, unhandledRejection and uncaughtException errors handled.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
-const server = require('./app');
+const app = require('./app');
 
-console.log(server.get('env'));
+console.log(app.get('env'));
 // console.log(process.env);
 
 const DB = process.env.DATABASE.replace(
@@ -20,6 +27,14 @@ mongoose
   });
 
 const port = process.env.PORT;
-server.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });

9331f2243b4656f1693345443c62e6cf3d284772
Hasan Yeniada <hasanyeniada@gmail.com>
2022-10-24 18:23:13 +0300

    Authentication & Authorization - rateLimiter & setting security HTTP headers added using helmet and express-rate-limit libs.

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 
 const sportCenterRoutes = require('./routes/sportCenterRoutes');
 const userRoutes = require('./routes/userRoutes');
 const globalErrorHandler = require('./middlewares/globalErrorHandler');
+const rateLimiter = require('./middlewares/rateLimiter');
 const AppError = require('./utils/appError');
 
 const app = express();
 
-// 1) Built-in Middlewares
+// 1) Global - Built-in Middlewares
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+
+// Limit requests from same API
+app.use('/api', rateLimiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
-// 2) Built-in Middlewares
+// 2) Custom Middlewares
 app.use((req, resp, next) => {
   console.log("Hello from custom middleware...");
   next();

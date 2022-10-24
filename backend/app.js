70b3f2ee4a061ba4d64b7be01759d32e08dda5f6
Hasan Yeniada <hasanyeniada@gmail.com>
2022-10-25 00:14:07 +0300

    Authentication & Authorization - data sanitization & preventing parameter pollution added.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const sportCenterRoutes = require('./routes/sportCenterRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -31,8 +34,25 @@ app.use('/api', rateLimiter);
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
+// Data Sanitization against NoSql Query Injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
+
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
+
+// Prevent Parameter Pollution
+app.use(hpp());
+
+app.use(
+  hpp({
+    whitelist: ['duration'],
+  })
+);
+
+
 
 // 2) Custom Middlewares
 app.use((req, resp, next) => {
